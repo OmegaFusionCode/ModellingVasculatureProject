1f780b76a8c8ce87e50dfbb2266af8e054b87f0c
OmegaFusionCode <39763713+OmegaFusionCode@users.noreply.github.com>
2021-11-10 14:19:29 +0000

    Removed some redundant code.

--- main/python/VascularTree.py
+++ main/python/VascularTree.py
@@ -38,28 +38,18 @@ class VascularTree:
         return self.domain.sample_discretised_points(h)
 
     def cost(self) -> float:
-        #total = 0.0
-        #for v in self.vessels:
-        #    r = v.radius
-        #    xp, yp = v.proximal_point
-        #    xd, yd = v.distal_point
-        #    length = math.sqrt((xp - xd) ** 2 + (yp - yd) ** 2)
-        #    total += r ** 2 * length
-        #return total
         return sum(v.cost for v in self.vessels)
 
     def next_vascular_tree(self) -> VascularTree:
         costs = []
         min_cost = None
-        vb = None
-        xb = None
         best_tree = None
         while len(costs) == 0:
             xdi = self.domain.generate_point()
             #xn = self.nearest_point_to(xdi)
-            # TODO: Compute the nearest point to xdi
+            # TODO: Compute the nearest point to xdi.
             reachable = self.vessels  #self.vessels_reachable_from(xdi)
-            # TODO: Vessels should not be reachable.
+            # TODO: Ignore unreachable vessels early.
             for vj in reachable:
                 xjs = self.get_candidate_bifurcation_points(xdi, vj)
                 for xj in xjs:
@@ -67,8 +57,6 @@ class VascularTree:
                     c = new_tree.cost()
                     if min_cost is None or c < min_cost:
                         min_cost = c
-                        vb = vj
-                        xb = xj
                         best_tree = new_tree
                     costs.append(c)
         return best_tree
