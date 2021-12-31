5245ec7da31da00584602ad8e6772167d252a945
OmegaFusionCode <39763713+OmegaFusionCode@users.noreply.github.com>
2021-12-31 15:58:40 +0000

    Renamed unit test files.

--- main/python/unittests/BloodVesselUnitTests.py
+++ main/python/unittests/BloodVesselUnitTests.py
@@ -4,7 +4,7 @@ from BloodVessel import Origin, BloodVessel
 from LinAlg import Vec2D
 
 
-class BloodVesselTesting(unittest.TestCase):
+class TestBloodVessel(unittest.TestCase):
 
     def test_something(self):
         r = Origin(0.5, Vec2D(1.0, 2.0))
@@ -51,9 +51,9 @@ class BloodVesselTesting(unittest.TestCase):
     def test_copy_subtree(self):
         r = Origin(0.5, Vec2D(1.0, 2.0))
         v1 = r.create_child(0.5, Vec2D(3.0, 4.0))
-        v2 = v1.create_child(0.25, Vec2D(3.5, 3.6))
+        v1.create_child(0.25, Vec2D(3.5, 3.6))
         v3 = v1.create_child(0.3, Vec2D(4.5, 3.9))
-        v4 = v3.create_child(0.2, Vec2D(9.9, 8.8))
+        v3.create_child(0.2, Vec2D(9.9, 8.8))
         descendants_old = list(r.descendants)
         descendants_new = list(r.copy_subtree().descendants)
         #for d1, d2 in zip(descendants_old, descendants_new):
