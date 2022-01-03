e89f38053d366e3f33f9392f72d1224044d05821
OmegaFusionCode <39763713+OmegaFusionCode@users.noreply.github.com>
2022-01-03 16:18:25 +0000

    origin is now a property of CCONetworkMaker
    This is to prepare for calculated rejection of generated points (which depend on the value of origin).

--- main/python/CCONetworkMaker.py
+++ main/python/CCONetworkMaker.py
@@ -12,52 +12,49 @@ class CCONetworkMaker:
         self.initial_point = initial_point
         self.flow = flow
         self.domain = domain
+        self._origin = None
 
     def _make_first_vessel(self) -> BaseBloodVessel:
         """Make the first vessel of the tree to start. """
-        origin = Origin(self.radius, self.initial_point)
+        self._origin = Origin(self.radius, self.initial_point)
         p = self.domain.generate_point()
-        origin.create_child(1.0, p)
-        return origin
+        self._origin.create_child(1.0, p)
 
     def generate_trees(self, iterations: int) -> Generator[BaseBloodVessel]:
         """A generator to produce the trees at all stages. """
-        origin = self._make_first_vessel()
+        self._make_first_vessel()
         if iterations > 0:
-            yield origin
+            yield self._origin
         for _ in range(1, iterations):
             # Rescaling happens here.
-            xd = self.domain.generate_point()   # Randomly select a terminal point to be connected to the tree.
-            #vj, xp = CCONetworkMaker.find_bifurcation(root, self.domain)
+            xd = self.domain.generate_point()  # Randomly select a terminal point to be connected to the tree.
             # TODO: Find the best bifurcation point.
-            # For now, just choose any vessel.
-            origin = origin.copy_subtree()
-            #vj = new_origin.root
+            self._origin = self._origin.copy_subtree()
             min_c = None
             best_vj = None
-            for vj in list(origin.descendants):
+            for vj in list(self._origin.descendants):
                 vj.bifurcate(xd)
                 bifurcated_vessels = vj.parent.children + [vj.parent]
                 vt = vj.parent.children[1]
                 assert len(bifurcated_vessels) == 3 and vj is bifurcated_vessels[0] and vt is bifurcated_vessels[1]
                 intersection_found = False
-                for w in origin.descendants:
+                for w in self._origin.descendants:
                     if w not in bifurcated_vessels and vt.line_seg.intersects_with(w.line_seg):
                         # Don't consider this bifurcation further if it intersects with other vessels
                         intersection_found = True
                 if intersection_found:
                     vj.remove_bifurcation()
                     continue
-                c = origin.root.cost
+                c = self._origin.root.cost
                 if min_c is None or c < min_c:
                     min_c = c
                     best_vj = vj
-                assert origin.root is not vj
+                assert self._origin.root is not vj
                 vj.remove_bifurcation()
             assert best_vj is not None
             best_vj.bifurcate(xd)
             # TODO: Bifurcation
-            yield origin
+            yield self._origin
             # TODO: Geometric Optimisation
 
     def run(self, terminals: int) -> BaseBloodVessel:
