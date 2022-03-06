85d5aafb71d57a7d9ad14150941c470e7da75e86
OmegaFusionCode <39763713+OmegaFusionCode@users.noreply.github.com>
2022-03-06 16:49:50 +0000

    Improved trimming of dead ends.
    Dead end trimming is slow, and this needs to be investigated, but the program now produces the desired output.

--- InvasionPercolationDrawingApp.py
+++ InvasionPercolationDrawingApp.py
@@ -4,7 +4,7 @@ from InvasionPercolationNetwork import InvasionPercolationNetwork
 
 
 class InvasionPercolationDrawingApp:
-    INTERVAL = 25
+    INTERVAL = 10
 
     def __init__(self, x, y, occ):
         self.x = x
@@ -13,25 +13,40 @@ class InvasionPercolationDrawingApp:
         pg.display.set_mode((x * self.INTERVAL, y * self.INTERVAL))
         self.surface = pg.display.get_surface()
         self.network = net = InvasionPercolationNetwork(x, y, occ)
-        self.failures_top_left, self.top_left = net.find_top_left(net.cells)
-        self.failures_bottom_right, self.bottom_right = net.find_bottom_right(net.cells)
-        a = net.bfs(self.top_left)
-        print("Reached")
-        self.shortest_path_edges = []
-        curr = self.bottom_right
-        while curr is not self.top_left:
-            curr, e = a[curr.i][curr.j]
-            self.shortest_path_edges.append(e)
+        self.failures_top_left, self.top_left = net.find_top_left()
+        self.failures_bottom_right, self.bottom_right = net.find_bottom_right()
+        #a = net.bfs(self.top_left)
+        #self.shortest_path_edges = []
+        #curr = self.bottom_right
+        #while curr is not self.top_left:
+        #    curr, e = a[curr.i][curr.j]
+        #    self.shortest_path_edges.append(e)
         self.remote_distance, self.remote_cell = net.find_most_distant_point(net.cells)
-        print(self.remote_distance)
-        print(self.remote_cell.i, self.remote_cell.j)
-        self.not_dead_end = net.remove_dead_ends(net.cells)
+        #print(self.remote_distance)
+        #print(self.remote_cell.i, self.remote_cell.j)
+        #for c, es in net.adjacency_list.items():
+        #    print(f"({c.i},{c.j}) - {es}")
 
 
     def get_coords(self, a):
         x_coord = self.INTERVAL // 2 + a.i * self.INTERVAL
         y_coord = self.INTERVAL // 2 + a.j * self.INTERVAL
         return x_coord, y_coord
+
+    def _draw_vertex_circles(self, cells, colour):
+        for c in cells:
+            pg.draw.circle(surface=self.surface,
+                           color=colour,
+                           radius=self.INTERVAL // 2,
+                           center=self.get_coords(c))
+
+    def _draw_edge_lines(self, edges, colour):
+        for e in edges:
+            pg.draw.line(surface=self.surface,
+                         color=colour,
+                         width=self.INTERVAL // 5,
+                         start_pos=self.get_coords(e.a),
+                         end_pos=self.get_coords(e.b))
 
     def draw(self):
         #for c in self.failures_top_left + self.failures_bottom_right:
@@ -45,30 +60,22 @@ class InvasionPercolationDrawingApp:
         #                   radius=self.INTERVAL // 2,
         #                   center=self.get_coords(c))
         # Geometrically Most Remote Point
-        for i, row in enumerate(self.not_dead_end):
-            for j, c in enumerate(row):
-                if c is not None:
-                    pg.draw.circle(surface=self.surface,
-                                   color=(255, 255, 0),
-                                   radius=self.INTERVAL // 2,
-                                   center=self.get_coords(c))
-
-        pg.draw.circle(surface=self.surface,
-                       color=(255, 0, 255),
-                       radius=self.INTERVAL // 2,
-                       center=self.get_coords(self.remote_cell))
-        for e in self.network.edges:
-            pg.draw.line(surface=self.surface,
-                         color=(255, 0, 0),
-                         width=self.INTERVAL // 5,
-                         start_pos=self.get_coords(e.a),
-                         end_pos=self.get_coords(e.b))
-        for e in self.shortest_path_edges:
-            pg.draw.line(surface=self.surface,
-                         color=(0, 255, 0),
-                         width=self.INTERVAL // 5,
-                         start_pos=self.get_coords(e.a),
-                         end_pos=self.get_coords(e.b))
+        #for i, row in enumerate(self.network.remove_dead_ends(self.network.cells)):
+        #    for j, c in enumerate(row):
+        #        if c is not None:
+        #            pg.draw.circle(surface=self.surface,
+        #                           color=(255, 255, 0),
+        #                           radius=self.INTERVAL // 2,
+        #                           center=self.get_coords(c))
+        self._draw_vertex_circles((self.remote_cell,), (255, 0, 255))
+        self._draw_edge_lines(self.network.edges, (255, 0, 0))
+        self._draw_edge_lines(self.network.remove_dead_ends(), (0, 255, 0))
+        #for e in self.network.shortest_path_edges:
+        #    pg.draw.line(surface=self.surface,
+        #                 color=(0, 255, 0),
+        #                 width=self.INTERVAL // 5,
+        #                 start_pos=self.get_coords(e.a),
+        #                 end_pos=self.get_coords(e.b))
         pg.display.flip()
 
     def run(self):
@@ -82,5 +89,5 @@ class InvasionPercolationDrawingApp:
 
 
 if __name__ == "__main__":
-    app = InvasionPercolationDrawingApp(20, 20, 0.55)
+    app = InvasionPercolationDrawingApp(50, 50, 0.55)
     app.run()
--- InvasionPercolationNetwork.py
+++ InvasionPercolationNetwork.py
@@ -1,7 +1,11 @@
-from queue import PriorityQueue, Queue
+from queue import PriorityQueue, Queue, LifoQueue
 from random import random
 
 import numpy as np
+
+
+def concat(xs):
+    return [y for x in xs for y in x]
 
 
 class Cell:
@@ -18,12 +22,21 @@ class Cell:
         return self.c < other.c
 
     @property
+    def indices(self):
+        return self.i, self.j
+
+    @property
     def is_discovered(self):
         return self.discovered != -1
 
     @property
     def is_reached(self):
         return self.reached != -1
+
+    def edge_to(self, other):
+        edges = [e for e in self.edges if e.touches(other)]
+        assert len(edges) == 1
+        return edges[0]
 
 
 class Edge:
@@ -33,6 +46,13 @@ class Edge:
         self.b = b
         a.edges.append(self)
         b.edges.append(self)
+
+    def touches(self, c):
+        return self.a is c or self.b is c
+
+    def other(self, c):
+        assert c is self.a or c is self.b
+        return self.b if c is self.a is c else self.a
 
 
 class InvasionPercolationNetwork:
@@ -49,11 +69,22 @@ class InvasionPercolationNetwork:
 
     @property
     def cells(self):
+        """A 2D list of the cells in the graph. """
         return self._cells
 
     @property
+    def nodes(self):
+        return concat([c.indices for c in self._cells])
+
+    @property
     def edges(self):
+        """A 2D list of the edges in the graph. """
         return self._edges
+
+    @property
+    def adjacency_list(self):
+        """Return an adjacency list in the form of a dictionary. """
+        return {c: [e.a if e.a is not c else e.b for e in c.edges] for c in concat(self._cells)}
 
     def _generate_random_capacities(self):
         return [[random() for _ in range(self.y)] for _ in range(self.x)]
@@ -112,62 +143,170 @@ class InvasionPercolationNetwork:
             self._discover_cells(neighbours, t)
             self._add_edges(neighbours, cell)
 
-    def find_top_left(self, cells):
+    @property
+    def top_left(self):
+        _, res = self.find_top_left()
+        assert res.is_reached
+        return res
+
+    def find_top_left(self):
         # TODO: Currently only works on square networks!
         failures = []
         for i in range(self.x):
             for j in range(i + 1):
-                c = cells[i - j][j]
+                c = self._cells[i - j][j]
                 if c.is_reached:
                     return failures, c
                 failures.append(c)
 
-    def find_bottom_right(self, cells):
+    @property
+    def bottom_right(self):
+        _, res = self.find_bottom_right()
+        assert res.is_reached
+        return res
+
+    def find_bottom_right(self):
         # TODO: Currently only works on square networks!
         failures = []
         for i in range(self.x):
             for j in range(i + 1):
-                c = cells[self.x - i + j - 1][self.y - j - 1]
+                c = self._cells[self.x - i + j - 1][self.y - j - 1]
                 if c.is_reached:
                     return failures, c
                 failures.append(c)
 
     def bfs(self, start):
         q = Queue()
-        distances = [[None for _ in range(self.y)] for _ in range(self.x)]
-        backrefs = [[None for _ in range(self.y)] for _ in range(self.x)]
-        distances[start.i][start.j] = 0
+        adj = self.adjacency_list
+        distances = {c: None for c in concat(self._cells)}
+        backrefs = {c: None for c in concat(self._cells)}
+        distances[start] = 0
         q.put(start)
         while not q.empty():
             u = q.get()
-            for e in u.edges:
-                v = e.a if e.a is not u else e.b  # Get the other end of this edge.
-                if distances[v.i][v.j] is None:
-                    distances[v.i][v.j] = distances[u.i][u.j] + 1
-                    backrefs[v.i][v.j] = u, e
+            for v in adj[u]:
+                if distances[v] is None:
+                    distances[v] = distances[u] + 1
+                    backrefs[v] = u
                     q.put(v)
         return backrefs
 
-    def remove_dead_ends(self, cells):
-        # TODO: Make a real cell not a dummy cell
-        not_dead_end = [[c for c in row] for row in cells]     # Copy cells
-        for i, row in enumerate(cells):
-            for j, c in enumerate(row):
-                if not c.is_reached:
-                    not_dead_end[c.i][c.j] = None
-        cells_to_expand = set()
-        for row in cells:
-            for c in row:
-                cells_to_expand.add(c)
-        # cells_to_expand now contains all cells
-        while len(cells_to_expand) > 0:
-            c = cells_to_expand.pop()
-            ns = [n for n in self._get_cell_neighbours(c) if n.is_reached]
-            if len(ns) == 1:   # Then it is a dead end
-                print("Reached!")
-                not_dead_end[c.i][c.j] = None  # Don't include the cell at this position
-                cells_to_expand.add(ns[0])
-        return not_dead_end
+    def remove_dead_ends(self):
+
+        source = self.top_left
+        sink = self.bottom_right
+        adj = self.adjacency_list
+
+        deleted = {e: False for e in self._edges}
+        nodes_deleted = {c: False for c in concat(self._cells)}
+
+        def can_find(start, no_visit):
+            # Can we find the source or the sink without ever visiting the node no_visit
+            s = LifoQueue()
+            discovered = {c: False for c in concat(self._cells)}
+            discovered[start] = True
+            s.put(start)
+            #print(f"{start.i} {start.j} ", end="")
+            while not s.empty():
+                u = s.get()
+                if u is source or u is sink:
+                    #print("found")
+                    return True
+                for v in adj[u]:
+                    if v is not no_visit and not discovered[v]:
+                        discovered[v] = True
+                        s.put(v)
+            #print("not found")
+            return False
+
+        def delete_scc_conditional(start, no_visit):
+            if not can_find(start, no_visit):
+                s = LifoQueue()
+                nodes_deleted[start] = True
+                s.put(start)
+                while not s.empty():
+                    u = s.get()
+                    for v in adj[u]:
+                        if v is not no_visit and not nodes_deleted[v]:
+                            nodes_deleted[v] = True
+                            s.put(v)
+
+        for e in self._edges:
+            #print(f"({e.a.i},{e.a.j}) ({e.b.i},{e.b.j}) ", end="")
+            if not deleted[e]:
+                #print("not deleted ", end="")
+                delete_scc_conditional(e.a, e.b)
+                delete_scc_conditional(e.b, e.a)
+                #if not can_find(e.a, e.b):
+                #    # We need to delete this strongly connected component.
+                #    print("\tdeleting left ")
+                #    s = LifoQueue()
+                #    nodes_deleted[e.a] = True
+                #    s.put(e.a)
+                #    while not s.empty():
+                #        u = s.get()
+#
+                #        for uv in u.edges:
+                #            v = uv.other(u)
+                #            print(f"\t({uv.a.i}, {uv.a.j}), ({uv.b.i}, {uv.b.j})")
+                #            # If this edge hasn't been deleted yet, delete it and delete its subgraph.
+                #            if v is not e.a and uv is not e and not deleted[uv]:
+                #                deleted[uv] = True
+                #                s.put(uv.other(u))
+                #if not can_find(e.b, e.a):
+                #    # We need to delete this strongly connected component.
+                #    s = LifoQueue()
+                #    s.put(e.b)
+                #    while not s.empty():
+                #        print("deleting right ", end="")
+                #        u = s.get()
+                #        for uv in u.edges:
+                #            v = uv.other(u)
+                #            # If this edge hasn't been deleted yet, delete it and delete its subgraph.
+                #            if v is not e.a and uv is not e and not deleted[uv]:
+                #                deleted[uv] = True
+                #                s.put(uv.other(u))
+                #print("")
+            #else:
+                #print("deleted")
+
+        return [e for e in self._edges if not nodes_deleted[e.a] and not nodes_deleted[e.b]]
+        #return [e for e in self._edges if not deleted[e]]
+
+
+    @property
+    def shortest_path_edges(self):
+        start = self.top_left
+        backrefs = self.bfs(start)
+        succ = self.bottom_right
+        edges = []
+        while succ is not start:
+            pred = backrefs[succ]
+            assert pred is not None
+            edges.append(pred.edge_to(succ))
+            succ = pred
+        return edges
+
+    #def remove_dead_ends(self, cells):
+    #    # TODO: Make a real cell not a dummy cell
+    #    not_dead_end = [[c for c in row] for row in cells]  # Copy cells
+    #    for i, row in enumerate(cells):
+    #        for j, c in enumerate(row):
+    #            if not c.is_reached:
+    #                not_dead_end[c.i][c.j] = None
+    #    cells_to_expand = set()
+    #    for row in cells:
+    #        for c in row:
+    #            cells_to_expand.add(c)
+    #    # cells_to_expand now contains all cells
+    #    while len(cells_to_expand) > 0:
+    #        c = cells_to_expand.pop()
+    #        ns = [n for n in self._get_cell_neighbours(c) if n.is_reached]
+    #        if len(ns) == 1:  # Then it is a dead end
+    #            print("Reached!")
+    #            not_dead_end[c.i][c.j] = None  # Don't include the cell at this position
+    #            cells_to_expand.add(ns[0])
+    #    return not_dead_end
 
     def find_most_distant_point(self, cells):
         pq = PriorityQueue()
